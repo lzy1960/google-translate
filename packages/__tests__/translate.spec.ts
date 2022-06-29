5e5c176cc9e287d7315b7615613e5ba134da3662
lzy1960 <1960553899@qq.com>
2022-06-29 17:45:42 +0800

    测试node-fetch formData

--- packages/__tests__/translate.spec.ts
+++ packages/__tests__/translate.spec.ts
@@ -2,16 +2,9 @@ import { describe, expect, test } from 'vitest'
 import { fetchTranslateInfo, translate } from '../src'
 
 describe('translate', () => {
-  test('should return object with text', () => {
-    const result = translate('你好')
-    const result1 = translate('是的')
-    expect(result.text).toBe('Hello')
-    expect(result1.text).toBe('Yes')
-  })
-
   test.only('should fetch google api and return result', async () => {
     const result = await fetchTranslateInfo('你好')
     console.log(result)
-    expect(result).toBe('Hello')
+    expect(result).toMatchSnapshot()
   })
 })
--- packages/src/index.ts
+++ packages/src/index.ts
@@ -5,38 +5,51 @@ export const translate = (text) => {
   return { text: 'Hello' }
 }
 
-export const fetchTranslateInfo = (text) => {
+export const fetchTranslateInfo = async (text) => {
   const url = 'https://translate.google.cn'
   const options = {
-    'source-path': 'text',
-    'f.sid': '-7518334055562924546',
-    bl: 'boq_translate-webserver_20220626.14_p0',
+    rpcids: 'MkEWBc',
+    'source-path': '/',
+    'f.sid': '6886843866985976507',
+    bl: 'boq_translate-webserver_20220627.09_p0',
     hl: 'zh-CN',
-    'soc-app': 1,
-    'soc-platform': 1,
-    'soc-device': 1,
-    _reqid: Math.floor(1000 + Math.random() * 9000),
+    'soc-app': '1',
+    'soc-platform': '1',
+    'soc-device': '1',
+    _reqid: '248103',
     rt: 'c',
   }
 
   const fullUrl =
     url + '/_/TranslateWebserverUi/data/batchexecute?' + qs.stringify(options)
-  console.log(fullUrl)
+  const params = new URLSearchParams()
+  params.append(
+    'f.req',
+    encodeURIComponent(
+      `[[["MkEWBc","[[\"你好\",\"auto\",\"en\",true],[null]]",null,"generic"]]]`
+    )
+  )
 
-  return fetch(fullUrl, {
+  console.log(params)
+
+  const res = await fetch(fullUrl, {
     method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    },
-    body: `f.req=${encodeURIComponent(
-      '[[["MkEWBc","[["你好","auto","en",true],[null]]",null,"generic"]]]'
-    )}&`,
+    // headers: {
+    //   'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    // },
+    body: params,
   })
-    .then((res) => {
-      console.log(res.text)
-      return res.text()
-    })
-    .then((res) => {
-      return res
-    })
+  const data = await res.text()
+  // )]}'
+  //
+  // 562
+  // [["wrb.fr","MkEWBc","[[\"Nǐ hǎo\",null,\"zh-CN\",[[[0,[[[null,2]],[true]]]],2],[[\"你好\",null,null,2]]],[[[null,null,null,null,null,[[\"Hello\",null,null,null,[[\"Hello\",[4,5,11]],[\"Hi\",[4]],[\"Hello there\",[4]]]]]]],\"en\",1,\"zh-CN\",[\"你好\",\"auto\",\"en\",true]],\"zh-CN\",[\"你好!\",null,null,null,null,[[[\"感叹词\",[[\"Hello!\",null,[\"你好!\",\"喂!\"],1,true],[\"Hi!\",null,[\"嗨!\",\"你好!\"],1,true],[\"Hallo!\",null,[\"你好!\"],3,true]],\"en\",\"zh-CN\"]],3],null,null,\"zh-CN\",1]]",null,null,null,"generic"],["di",26],["af.httprm",26,"2345273230130846814"]]
+  // 25
+  // [["e",4,null,null,634]]
+
+  // 最上面的两行去掉以后
+  // 总共四行
+  // 第一行表示前两行的长度
+  // 第三行表示后两行的长度
+  return data
 }
